25ac106b5279de85a1251d23aaa0c3e0b00734c1
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2024-11-16 10:21:40 +0100

    cleanup

--- rag-demo-backend/RagDemoAPI/Extensions/ChatMessageCitationExtensions.cs
+++ rag-demo-backend/RagDemoAPI/Extensions/ChatMessageCitationExtensions.cs
@@ -6,7 +6,6 @@ public static class ChatMessageCitationExtensions
 {
 #pragma warning disable AOAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
     public static List<RetrievedDocument> ToChatMessageCitations(this IEnumerable<Azure.AI.OpenAI.Chat.ChatCitation> azureChatCitations)
-#pragma warning restore AOAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
     {
         var citations = new List<RetrievedDocument>();
 
